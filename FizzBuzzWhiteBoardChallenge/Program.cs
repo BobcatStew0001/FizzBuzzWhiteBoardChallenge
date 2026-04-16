33e42b432fb6e26b7721fb4df0f5b0e328475148
Zach Brown <bigcountry66789@gmail.com>
2026-04-15 20:16:37 -0500

    Test

---
+++ FizzBuzzWhiteBoardChallenge/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    public static string DivisibleBy(int n)
+    {
+        if(n % 3 == 0 && n % 5 == 0)
+        {
+            return "FizzBuzz"; 
+        }
+        else if(n % 3 == 0)
+        {
+            return "Fizz"; 
+        }
+        else if(n % 5 == 0)
+        {
+            return "Buzz";
+        }
+        else
+        {
+            return "This number is not divisible by 3 or 5.";
+        }
+    }
+static void Main(string[] args)
+{
+    System.Console.WriteLine($"{DivisibleBy(15)}");
+    System.Console.WriteLine($"{DivisibleBy(9)}");
+    System.Console.WriteLine($"{DivisibleBy(10)}");
+    System.Console.WriteLine($"{DivisibleBy(7)}");
+}
+
+        
+}
